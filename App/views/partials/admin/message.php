53b06400074cc2dfe838bf262e9080cf3433d20c
mori3nt3z <mori3nt3z@gmail.com>
2024-03-15 13:23:34 +0330

    last

--- App/views/partials/admin/message.php
+++ App/views/partials/admin/message.php
@@ -20,21 +20,24 @@ if(isset($_SESSION['userRegisteredSuccessfully'])){
     echo '<div class="alert alert-success">'.$_SESSION["userRegisteredSuccessfully"].'</div>';
     unset($_SESSION['userRegisteredSuccessfully']);
 }
+
 if(isset($_SESSION['youHaveNotPermission'])){
     echo '<div class="alert alert-danger">'.$_SESSION["youHaveNotPermission"].'</div>';
     unset($_SESSION['youHaveNotPermission']);
 }
+
 if(isset($_SESSION['VideoUploadedSuccessfully'])){
     echo '<div class="alert alert-success">'.$_SESSION["VideoUploadedSuccessfully"].'</div>';
     unset($_SESSION['VideoUploadedSuccessfully']);
 }
+
 if(isset($_SESSION['completeUserInfo'])){
     echo '<div class="alert alert-danger">'.$_SESSION["completeUserInfo"].'</div>';
     unset($_SESSION['completeUserInfo']);
 }
+
 if(isset($_SESSION['userCompleteInfoSuccessfully'])){
     echo '<div class="alert alert-success">'.$_SESSION["userCompleteInfoSuccessfully"].'</div>';
     unset($_SESSION['userCompleteInfoSuccessfully']);
 }
 
-//userRegisteredSuccessfully
